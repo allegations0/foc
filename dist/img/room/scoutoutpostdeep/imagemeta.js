34b334cef01659791a9a9b6597bf7d90be2d07c8
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-05-30 01:01:33 +0700

    Scout outpost deep, scout tunnel, scout duty images

---
+++ dist/img/room/scoutoutpostdeep/imagemeta.js
@@ -0,0 +1,14 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "scoutoutpostdeep.png": {
+      title: "Scout Outpost Deep",
+      artist: "darko",
+      url: "https://gitgud.io/darkofocdarko/fort-of-chains/-/blob/master/docs/tileset_credits.md",
+      license: "CC-BY-SA 3.0",
+      extra: "See URL for complete tileset credits",
+      directional: true,
+    },
+  }
+
+}());
---
+++ dist/img/room/scouttunnel/imagemeta.js
@@ -0,0 +1,15 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "scouttunnel.png": {
+      title: "Scout Tunnel",
+      artist: "darko",
+      url: "https://gitgud.io/darkofocdarko/fort-of-chains/-/blob/master/docs/tileset_credits.md",
+      license: "CC-BY-SA 3.0",
+      extra: "See URL for complete tileset credits",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
