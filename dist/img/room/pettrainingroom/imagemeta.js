5d868cafb0d34bf4bee106e44d8147c5cf2bd6c0
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-06-04 16:49:20 +0700

    Sissy, pet, vaginal training room images (by toyRubberDucky)

---
+++ dist/img/room/pettrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "pettrainingroom.png": {
+      title: "Pet Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
---
+++ dist/img/room/vaginatrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "vaginatrainingroom.png": {
+      title: "Vagina Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
