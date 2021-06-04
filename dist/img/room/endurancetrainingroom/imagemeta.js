19f643683443fc74557ac657b6a74b2c38a38fb1
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-06-04 20:23:57 +0700

    Endurance, horny, obedience training room images by toyRubberDucky

---
+++ dist/img/room/endurancetrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "endurancetrainingroom.png": {
+      title: "Endurance Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
---
+++ dist/img/room/hornytrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "hornytrainingroom.png": {
+      title: "Horny Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
