65d297e0504c60f801d7fdb733dbc7b740feed75
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-06-04 18:03:50 +0700

    Dominance and Masochist training room images (by toyRubberDucky)

---
+++ dist/img/room/dominancetrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "dominancetrainingroom.png": {
+      title: "Dominance Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
---
+++ dist/img/room/masochisttrainingroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "masochisttrainingroom.png": {
+      title: "Masochist Training Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
