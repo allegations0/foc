a6ff7fdad1717ee727126818f133391477494fa3
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-06-04 15:32:55 +0700

    Milker Room, Anal and Oral training room images (by toyRubberDucky)

---
+++ dist/img/room/milkerroom/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "milkerroom.png": {
+      title: "Milker Room",
+      artist: "toyRubberDucky",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+      nowalls: true,
+    },
+  }
+
+}());
