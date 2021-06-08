56663ee081e32083e58d0f2aded2b15af689edcb
darkofoc <17909-darkofocdarko@users.noreply.gitgud.io>
2021-06-08 19:10:00 +0700

    Anal Fuckhole Room image by Picobyte

---
+++ dist/img/room/analfuckhole/imagemeta.js
@@ -0,0 +1,13 @@
+(function () {
+  /* Image credit information. */
+  ROOMIMAGE_CREDITS = {
+    "analfuckhole.png": {
+      title: "Anal Fuckhole",
+      artist: "Picobyte",
+      url: "",
+      license: "CC-BY-SA 3.0",
+      directional: true,
+    },
+  }
+
+}());
